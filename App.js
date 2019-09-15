70ba1fc79134e0c2bd58c3ab6f674cd3762a8c72
muskanaul <mk2aulak@edu.uwaterloo.ca>
2019-09-14 20:39:03 -0400

    update

---
+++ App.js
@@ -0,0 +1,82 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+import React, { PureComponent } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View, Vibration} from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import ReactNativeHapticFeedback from "react-native-haptic-feedback";
+import ReactNativeHaptic from 'react-native-haptic';
+
+const options = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: false
+};
+
+export default class App extends PureComponent {
+  handleVibration(event){
+   // console.log(`x coord = ${event.nativeEvent.locationX}, y coord = ${event.nativeEvent.locationY}` );
+  }
+  render() {
+    return (
+      <View style={styles.container} onMoveShouldSetResponder={(event) => console.log(`X Cord: ${event.nativeEvent.locationX}, Y Cord: ${event.nativeEvent.locationY}`)}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+        >
+      </RNCamera>
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => ReactNativeHaptic.generate('impactHeavy')} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> Vibrate </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  takePicture = async() => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
