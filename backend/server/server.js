2f8f3e304d83b8f0fbe72afbce7b93531e548333
Jim Yang <jim@v1040-wn-rt-a-39-126.campus-dynamic.uwaterloo.ca>
2019-09-15 01:19:23 -0400

    HTN-5 upload img, parse and return b64

--- backend/server/server.js
+++ backend/server/server.js
@@ -12,12 +12,24 @@ const app = express()
 const port = 3000
 const getDepth = require('./helpers/depth').getDepth
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(__dirname + '/uploads'));
 
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use(express.static('public'));
 
 app.post('/upload', upload.single('photo'), async (req, res) => {
+    let returnObj = {}
+
+    if(req.file) {
+        const temp = await getDepth(req.file.path)
+        returnObj.processed_url = req.get('Host') + `/images/${req.file.filename}_processed.jpg`
+        returnObj.processed_b64 = await fs.readFileSync(`./uploads/images/${req.file.filename}_processed.jpg`, 'base64');
+        res.json(returnObj);
+    }
+    else{
+        throw 'No file was submitted';
+    }
 
     azureInfo = await callAzure(__dirname+"/"+req.file.path);
     console.log(azureInfo)
@@ -27,14 +39,11 @@ app.post('/upload', upload.single('photo'), async (req, res) => {
 
 app.listen(port, () => console.log(`Example app listening on port1 ${port}!`))
 
-
 //headers for Azure/Axios
 const headers = {
     'Content-Type': 'application/octet-stream',
     'Ocp-Apim-Subscription-Key': process.env.API_KEY
-  }
-  
-  console.log(process.env.API_KEY);
+}
 
 //helper function for fetching azure to avoid merging issues
 async function callAzure(imageDestination){
